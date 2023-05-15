959459ae06b37aec46f46c539aac8c89b52ddf0c
xalzh <49931221+xalzh@users.noreply.github.com>
2023-05-15 14:02:11 -0400

    update imu.py with comments

--- IMU.py
+++ IMU.py
@@ -1,13 +1,21 @@
-import time
-import board
+"""
+5/11/2023 by Zehao Li
+
+This program defines a class for the BNO055 9-DOF sensor. The class provides functionality to initialize the sensor,
+set its parameters, and read different types of sensor data including temperature, Euler angles, quaternion,
+and calibration status.
+"""
 import Adafruit_BNO055.BNO055 as BNO055
-import math
+
 
 class bno055():
     def __init__(self):
         super()
+        # Initialize BNO055 sensor
         self.bno = BNO055.BNO055(busnum=1)
+        # Set update frequency
         self.BNO_UPDATE_FREQUENCY_HZ = 50
+        # Set axis remap for BNO055 sensor
         self.BNO_AXIS_REMAP = {
             'x': BNO055.AXIS_REMAP_X,
             'y': BNO055.AXIS_REMAP_Z,
@@ -21,15 +29,15 @@ class bno055():
         self.bno.set_axis_remap(**self.BNO_AXIS_REMAP)
 
     def read_bno_data(self):
+        # Read data from BNO055 sensor
         temp = self.bno.read_temp()
         heading, roll, pitch = self.bno.read_euler()
         x, y, z, w = self.bno.read_quaternion()
         sys, gyro, accel, mag = self.bno.get_calibration_status()
         status, self_test, error = self.bno.get_system_status(run_self_test=False)
-
         if error != 0:
             print("Error! Value: {0}".format(error))
-
+        # Pack all data into a dictionary for return
         data = {
             'euler': (heading, roll, pitch),
             'temp': temp,
