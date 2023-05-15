996944733692cf48554c1ae548950b7f0f853da4
xalzh <49931221+xalzh@users.noreply.github.com>
2023-05-15 14:19:24 -0400

    create current python project in html format

--- python_to_html.py
+++ python_to_html.py
@@ -3,21 +3,25 @@ from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
 
 # Path to the Python file you want to convert to HTML.
-python_file_path = 'main.py'
+files = ['camera.py', 'IMU.py', 'main.py', 'object_tracking.py', 'PID.py', 'reset.py', 'rolling_control.py', 'UI.py',
+         'Update_Screen.py', 'web.py']
 
-# Create a formatter with line numbers.
-formatter = HtmlFormatter(linenos=True)
+for file in files:
+    python_file_path = file
 
-# Read the Python code from the file.
-with open(python_file_path, 'r') as f:
-    code = f.read()
+    # Create a formatter with line numbers.
+    formatter = HtmlFormatter(linenos=True)
 
-# Highlight the code and convert it to HTML.
-html_code = highlight(code, PythonLexer(), formatter)
+    # Read the Python code from the file.
+    with open(python_file_path, 'r') as f:
+        code = f.read()
 
-# Write the HTML code to a file.
-with open('main.html', 'w') as f:
-    # HtmlFormatter().get_style_defs('.highlight') is used to include the necessary CSS definitions
-    f.write('<html><body><style type="text/css">' + formatter.get_style_defs('.highlight') + '</style>\n')
-    f.write(html_code)
-    f.write('</body></html>')
+    # Highlight the code and convert it to HTML.
+    html_code = highlight(code, PythonLexer(), formatter)
+
+    # Write the HTML code to a file.
+    with open('python_html/'+python_file_path[:-3]+".html", 'w') as f:
+        # HtmlFormatter().get_style_defs('.highlight') is used to include the necessary CSS definitions
+        f.write('<html><body><style type="text/css">' + formatter.get_style_defs('.highlight') + '</style>\n')
+        f.write(html_code)
+        f.write('</body></html>')
